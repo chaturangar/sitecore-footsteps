b0d135ab235e96a5bf1e07e84a67616baa31118b
chaturanga ranatunga <cra@codehousegroup.com>
2015-07-24 12:39:58 +0530

    modify script file to support document ready

--- SitecoreFootsteps.Modules/SitecoreFootsteps.Modules.QuickInfoTooltip/sitecore modules/SitecoreFootsteps/QuickInfoTooltip/Scripts/item.tooltip.js
+++ SitecoreFootsteps.Modules/SitecoreFootsteps.Modules.QuickInfoTooltip/sitecore modules/SitecoreFootsteps/QuickInfoTooltip/Scripts/item.tooltip.js
@@ -1,43 +1,45 @@
-﻿var me;
-var formatedId;
-var possisanLeft;
-var possisanTop;
-var newKay = "";
-var meanuWidth;
-var styles;
-var spanStyles;
-var substr;
+﻿jQuery(document).ready(function () {
+    var me;
+    var formatedId;
+    var possisanLeft;
+    var possisanTop;
+    var newKay = "";
+    var meanuWidth;
+    var styles;
+    var spanStyles;
+    var substr;
 
-jQuery('#ContentTreeInnerPanel').on('mouseenter', '.scContentTreeNode', function (event) {
-	me = jQuery(this);
-	formatedId = me.children().last().attr('id').substr(10).split("");
-	possisanLef = me.position().left;
-	possisanTop = me.position().top + 150;
-	menanuWidth = me.width();
+    jQuery('#ContentTreeInnerPanel').on('mouseenter', '.scContentTreeNode', function (event) {
+        me = jQuery(this);
+        formatedId = me.children().last().attr('id').substr(10).split("");
+        possisanLef = me.position().left;
+        possisanTop = me.position().top + 150;
+        menanuWidth = me.width();
 
-	getItemData(formatedId, possisanLef, possisanTop, menanuWidth);
-}).on('mouseleave', '.scContentTreeNode', function (event) {
-	newKay = "";
+        getItemData(formatedId, possisanLef, possisanTop, menanuWidth);
+    }).on('mouseleave', '.scContentTreeNode', function (event) {
+        newKay = "";
 
-	jQuery(".tooltipInfo").remove();
+        jQuery(".tooltipInfo").remove();
+    });
+
+    function getItemData(itemId, left, top, width) {
+        for (var i = 0; i < itemId.length; i++) {
+            newKay += itemId[i];
+            if (i == 7) { newKay += '-'; }
+            if (i == 11) { newKay += '-'; }
+            if (i == 15) { newKay += '-'; }
+            if (i == 19) { newKay += '-'; }
+        }
+
+        // Setting multiple properties.
+        left = left + width;
+        styles = "top: " + top + "px;left: " + left + "px; padding: 10px; border: solid 1px #474747; border-radius: 10px; z-index: 999; width: auto !important; position: absolute; background: #F0F0F0;"
+        spanStyles = "position: relative; margin-bottom: 4px; padding: 2px 0; display:block;";
+
+        console.log("test");
+        jQuery.get("/sitecore/api/ssc/item/" + newKay + "", function (data) {
+            jQuery(".scFlexColumnContainer", parent.document.body).append("<div style='" + styles + "' class='tooltipInfo'><span style='" + spanStyles + "'><strong>Item ID : </strong>" + data.ItemID + "</span><span style='" + spanStyles + "'><strong>Item Name :  </strong>" + data.ItemName + "</span><span style='" + spanStyles + "'><strong>Item Path :  </strong>" + data.ItemPath + "</span><span style='" + spanStyles + "'> <strong>Template Name :  </strong>" + data.TemplateName + "</span><span style='" + spanStyles + "'> <strong>Template ID :  </strong>" + data.TemplateID + "</span></div>").show();
+        });
+    }
 });
-
-
-function getItemData(itemId, left, top, width) {
-	for (var i = 0; i < itemId.length; i++) {
-		newKay += itemId[i];
-		if (i == 7) { newKay += '-'; }
-		if (i == 11) { newKay += '-'; }
-		if (i == 15) { newKay += '-'; }
-		if (i == 19) { newKay += '-'; }
-	}
-
-	// Setting multiple properties.
-	left = left + width;
-	styles = "top: " + top + "px;left: " + left + "px; padding: 10px; border: solid 1px #474747; border-radius: 10px; z-index: 999; width: auto !important; position: absolute; background: #F0F0F0;"
-	spanStyles = "position: relative; margin-bottom: 4px; padding: 2px 0; display:block;";
-
-	jQuery.get("http://chg.local/sitecore/api/ssc/item/" + newKay + "", function (data) {
-		jQuery(".scFlexColumnContainer", parent.document.body).append("<div style='" + styles + "' class='tooltipInfo'><span style='" + spanStyles + "'><strong>Item ID : </strong>" + data.ItemID + "</span><span style='" + spanStyles + "'><strong>Item Name :  </strong>" + data.ItemName + "</span><span style='" + spanStyles + "'><strong>Item Path :  </strong>" + data.ItemPath + "</span><span style='spanStyles'> <strong>Template Name :  </strong>" + data.TemplateName + "</span><span style='" + spanStyles + "'> <strong>Template ID :  </strong>" + data.TemplateID + "</span></div>").show();
-	});
-}
